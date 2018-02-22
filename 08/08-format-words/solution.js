160c80bac19222c0cc792f78929c3823bdbebc9a
Jeremy Crawford <jfcraw08@gmail.com>
2018-02-22 13:29:18 -0800

    kata/readme/image complete

---
+++ 08/08-format-words/solution.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function formatWords(words) {
+  let newArray = [];
+  
+  for (let i in words) {
+    if(words[i] !== '') {
+      newArray.push(words[i]);
+    }
+  }
+  if (newArray.length < 2) {
+    return newArray.join();
+  } else if (newArray.length === 2) {
+    let lastItem = newArray.pop();
+    return newArray.join() + ' and ' + lastItem;
+  } else {
+    let lastItem = newArray.pop();
+    return newArray.join(', ') + ' and ' + lastItem;
+  }
